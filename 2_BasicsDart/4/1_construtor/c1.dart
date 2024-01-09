e4137b98dc4380ed2ee29c32ef8e005046ef2a08
Amar Thonge <amarthonge6767@gmail.com>
2024-01-09 17:03:09 +0530

    Flutter rep Book (Flutter in action)

---
+++ 2_BasicsDart/4/1_construtor/c1.dart
@@ -0,0 +1,9 @@
+import '../../../../dart-prctice-codes/4ControlStatement/ASSIGNMET/5/q6.dart';
+
+class Test {
+  final int x;
+  final int y;
+  const Test()
+    Print("in constant contru");
+  }
+}
