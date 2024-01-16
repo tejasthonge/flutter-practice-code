c9834929ad45549499de4340c6cae91c5ac8aa36
Amar Thonge <amarthonge6767@gmail.com>
2024-01-16 14:44:37 +0530

    cheke The no. in list on pressed

---
+++ 5_Building_App/assingment9/palindrome_countapp/android/app/src/main/kotlin/com/example/palindrome_countapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.palindrome_countapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 5_Building_App/assingment9/palindrome_countapp/lib/assigment4.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class Assignment4 extends StatefulWidget {
+  const Assignment4({super.key});
+
+  @override
+  State<Assignment4> createState() => _Assignment4State();
+}
+
+class _Assignment4State extends State<Assignment4> {
+  int count = 0;
+  void countPalindrome() {
+    count = 0;
+    List<int> numbersList = [1, 225, -77, 121, 111, 234, 999];
+
+    for (int i = 0; i < numbersList.length; i++) {
+      int temp = numbersList[i].abs();
+      int reverseNum = 0;
+      while (temp != 0) {
+        reverseNum = reverseNum * 10 + temp % 10;
+        temp ~/= 10;
+      }
+      if (reverseNum == numbersList[i].abs()) {
+        count++;
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Assigment4"),
+      ),
+      body: SizedBox(
+          width: double.infinity,
+          child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        countPalindrome();
+                        setState(() {});
+                      },
+                      child: const Text("Check palindrome"),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text("$count Numbers are palindrome")
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Chek Strong"),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text("$count Numbers are stronge ")
+                    
+                  ],
+                ),
+              ])),
+    );
+  }
+}
